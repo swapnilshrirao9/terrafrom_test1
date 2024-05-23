67d453a609bfb174a8aad520799878053cb3fa48
azshrirao <azureshrirao@gmail.com>
2024-05-23 08:25:00 +0000

    add basic

---
+++ Module/create_repo/resource.tf
@@ -0,0 +1,7 @@
+resource "github_repository" "by_terrafrom" {
+  name        = "Test1"
+  description = "This repo created by terrfrom code"
+  visibility  = "private"
+
+}
+
--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
-provider "azurerm" {
-
+variable "github_token" {
+  type = string
 }
-resource "create_resouce" "name_of_create_resouce"{
-
-}
-resource "Vnet" "identify_on_statefile" {
-
+module "create_repo_git" {
+  source       = "./Modules/create_repo/"
+  github_token = var.github_token
 }
